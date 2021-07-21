e051207117e26201ef2acaf66486a6734ef616fd
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-07-21 15:23:39 -0300

    titans

---
+++ titans.py
@@ -0,0 +1,305 @@
+import os
+import time
+
+
+
+#\033[1;30m Branco
+#\033[1;31m Vermelho
+#\033[1;32m Verde
+#\033[1;33m Amarelo
+#\033[1;34m Azul
+#\033[1;35m Roxo
+#\033[1;36m Ciano
+#\033[1;37m Cinza Claro
+
+
+os.system("clear")
+os.system("pkg install figlet")
+os.system("clear")
+print("\033[1;36m")
+print("=--=--=--=--=--=--=--=--=--=--=--=--=--=")
+os.system("figlet Titans")
+print("=--=--=--=--=--=--=--=--=--=--=--=--=--=")
+print("""\033[1;33m
+l----------------------------l\033[1;35m
+l       [1]Somar             l
+l       [2]Multiplica        l
+l       [3]Install git       l
+l       [4]Install python3   l
+l       [5]Install python2   l
+l       [6]Install Update    l
+l       [7]Install upgrade   l
+l       [8]Install wget      l
+l       [9]Install nmap      l
+l       [10]Install su       l
+l       [11]Install tsu      l
+l       [12]Install tor      l
+l       [13]Install ruby     l
+l       [14]Install Hydra    l
+l       [15]Install php      l
+l       [16]Install proot    l
+l       [17]Install Perl     l
+l       [18]Install Curl     l
+l       [19]Install Clang    l
+l       [20]Install nano     l
+l       [21]jdash            l
+l       [22]Zorov1           l
+l       [23]F1GH             l
+l       [24]Cyber-Punk       l
+l       [25]INFECTING        l
+l       [26]Open-File        l
+l       [27]Of-Sploit        l
+l       [28]Cracker          l
+l       [29]Looting          l
+l       [30]Gato Dark V1     l
+l       [31]Lazy             l
+l       [32]Creditos         l
+l       [32]Lazymux          l
+l       [33]Kiny-Painel      l
+l       [34]FSOCIETY         l
+l       [35]Bot Zap Zop      l
+l       [36]Crêditos         l
+l       [37]Sair             l\033[1;33m
+l----------------------------l
+""")
+op = input("\033[1;31m>>> ")
+
+if op == "1":
+    os.system("clear")
+    n1 = int(input("Num1 "))
+    n2 = int(input("Num2 "))
+    somar = n1+n2
+    print("A soma entre {} e {} é {}".format(n1, n2, somar))
+    time.sleep(7)
+    os.system("python3 titans.py")
+elif op == "2":
+    os.system("clear")
+    n1 = int(input("Num1 "))
+    n2 = int(input("Num2 "))
+    res = n1*n2
+    print("Produto = {}×{} = {}".format(n1, n2, res))
+    time.sleep(7)
+    os.system("python3 titans.py")
+elif op == "3":
+    os.system("""
+    clear
+    pkg install git
+    python3 titans.py
+    """)
+elif op == "4":
+    os.system("""
+    clear
+    pkg install python3
+    python3 titans.py
+    """)
+elif op == "5":
+    os.system("""
+    clear
+    pkg install python2
+    python3 titans.py
+    """)
+elif op == "6":
+    os.system("""
+    clear
+    apt-get update
+    python3 titans.py
+    """)
+elif op == "7":
+    os.system("""
+    clear
+    apt-get update
+    y
+    python3 titans.py
+    """)
+elif op == "8":
+    os.system("""
+    clear
+    pkg install wget
+    python3 titans.py
+    """)
+elif op == "9":
+    os.system("""
+    clear
+    pkg install nmap
+    python3 titans.py
+    """)
+elif op == "10":
+    os.system("""
+    clear
+    pkg install su
+    python3 titans.py
+    """)#Part 1 Concluida!
+elif op == "11":
+    os.system("""
+    clear
+    pkg install tsu
+    python3 titans.py
+    """)
+elif op == "12":
+    os.system("""
+    clear
+    pkg install tor
+    python3 titans.py
+    """)
+elif op == "13":
+    os.system("""
+    clear
+    pkg install ruby
+    python3 titans.py
+    """)
+elif op == "14":
+    os.system("""
+    clear
+    pkg install hydra
+    python3 titans.py
+    """)
+elif op == "15":
+    os.system("""
+    clear
+    pkg install php
+    python3 titans.py
+    """)
+elif op == "16":
+    os.system("""
+    clear
+    pkg install proot
+    python3 titans.py
+    """)
+elif op == "17":
+    os.system("""
+    clear
+    pkg install perl
+    python3 titans.py
+    """)
+elif op == "18":
+    os.system("""
+    clear
+    pkg install curl
+    python3 titans.py
+    """)
+elif op == "19":
+    os.system("""
+    clear
+    pkg install clang
+    python3 titans.py
+    """)
+elif op == "20":
+    os.system("""
+    clear
+    pkg install nano
+    python3 titans.py
+    """)#Part 2 Concluida!
+elif op == "21":
+    os.system("""
+    clear
+    git clone https://github.com/sachin175638/jdash
+    python3 titans.py
+    """)
+elif op == "22":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/Zorov1.git
+    python3 titans.py
+    """)
+elif op == "23":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/F1GH.git
+    python3 titans.py
+    """)
+elif op == "24":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/Cyber-Punk.git
+    python3 titans.py
+    """)
+elif op == "25":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/INFECTING.git
+    python3 titans.py
+    """)
+elif op == "26":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/Open-File.git
+    python3 titans.py
+    """)
+elif op == "27":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/Of-Sploit.git
+    python3 titans.py
+    """)
+elif op == "28":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/CRACKER.git
+    python3 titans.py
+    """)
+elif op == "29":
+    os.system("""
+    clear
+    git clone https://github.com/ZoroScripter/Looting.git
+    python3 titans.py
+    """)
+elif op == "30":
+    os.system("""
+    clear
+    git clone https://github.com/Ranginang67/DarkFly-Tool
+    python3 titans.py
+    """)
+elif op == "31":
+    os.system("""
+    clear
+    git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git
+    python3 titans.py
+    """)
+elif op == "32":
+    os.system("""
+    clear
+    git clone https://github.com/Gameye98/Lazymux
+    python3 titans.py
+    """)
+elif op == "33":
+    os.system("""
+    clear
+    git clone https://github.com/Kiny-Kiny/Kiny-Painel
+    python3 titans.py
+    """)
+elif op == "34":
+    os.system("""
+    clear
+    git clone https://github.com/Manisso/fsociety
+    python3 titans.py
+    """)
+elif op == "35":
+    os.system("""
+    clear
+    git clone https://github.com/fdciabdul/termux-whatsapp-bot
+    python3 titans.py
+    """)
+elif op == "36":
+    os.system("clear")
+    print("""\033[1;31m
+    =-=-=-=-=-=
+    = Criador:=
+    =-=-=-=-=-=
+    ZoroScripter\033[1;35m
+    ==============
+    = Apoiadores =
+    ==============
+    Columbiaxxxs
+    w4li
+    """)
+    time.sleep(6)
+    os.system("python3 titans.py")
+elif op == "37":
+    print("\033[1;35m")
+    os.system("""
+    clear
+    figlet Saindo....
+    """)
+    exit()
+else:
+    os.system("python3 titans.py")
